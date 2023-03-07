6d77337e1ac8b9d83b0c813ba76bbd482c3ca538
raghu0056 <raghumani0056@gmail.com>
2023-03-07 07:33:35 +0530

    Employee waves calculation

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,19 @@
 import java.util.Random;
 
-public class Main {
-    public static void main(String[] args)
-    {
-        System.out.println("Please enter the number to check the employee is present or absent");
-        Random rand = new Random();
-        int n = rand.nextInt(20);
-        System.out.println(n);
-        if(n<10)
-        {
-            System.out.println("Employee is present");
-        } else {
-            System.out.println("Employee is absent");
+public class Main{
+    public static void main(String[] args) {
+        int emp = 4;
+        int Perhour = 20;
+        if (emp >5) {
+            int workinghours = 6;
+            int totalWaves = workinghours * Perhour;
+            System.out.println("The total working salary of employee is " +totalWaves +"rs");
         }
-
+        else {
+            int workinghours = 4;
+            int totalWaves = workinghours * Perhour;
+            System.out.println("The total working salary of employee is " +totalWaves +"rs");
+        }
     }
+
 }
